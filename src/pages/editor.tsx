90d4d14bbc509699883c6dd64da7bd086bb1d68a
yukoshan <yukorin0105@gmail.com>
2024-02-11 11:39:33 +0900

    4-3まで完了

---
+++ src/pages/editor.tsx
@@ -0,0 +1,103 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import { useStateWithStorage } from '../hooks/use_state_with_storage'
+import * as ReactMarkdown from 'react-markdown'
+import { putMemo } from '../indexeddb/memos'
+import { Button } from '../components/button'
+import { SaveModal } from '../components/save_modal'
+import { Link } from 'react-router-dom'
+import { Header } from '../components/header'
+   
+const { useState } = React
+
+
+
+// const HeaderControl = styled.div`
+//     height: 2rem;
+//     display: flex;
+//     align-content: center;
+//   `
+   
+const Wrapper = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 3rem;
+`
+
+const HeaderArea = styled.div`
+    position: fixed;
+    right: 0;
+    top: 0;
+    left: 0;
+    `
+
+const TextArea = styled.textarea`
+  border-right: 1px solid silver;
+  border-top: 1px solid silver;
+  bottom: 0;
+  font-size: 1rem;
+  left: 0;
+  padding: 0.5rem;
+  position: absolute;
+  top: 0;
+  width: 50vw;
+`
+
+const Preview = styled.div`
+  border-top: 1px solid silver;
+  bottom: 0;
+  overflow-y: scroll;
+  padding: 1rem;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 50vw;
+`
+
+interface Props {
+  text: string
+  setText: (text: string) => void
+}
+
+export const Editor: React.FC<Props> = (props) => {
+  const { text, setText } = props
+  const saveMemo = (): void => {
+    putMemo('TITLE', text)
+  }
+  const [showModal, setShowModal] = useState(false)
+ 
+  return (
+    <>
+      <HeaderArea>
+        <Header title="Markdown Editor">
+          <Button onClick={() => setShowModal(true)}>
+            保存する
+          </Button>
+          <Link to="/history">
+            履歴を見る
+          </Link>
+        </Header>
+      </HeaderArea>
+      <Wrapper>
+        <TextArea
+          onChange={(event) => setText(event.target.value)}
+          value={text}
+        />
+        <Preview>
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </Preview>
+      </Wrapper>
+      {showModal && (
+          <SaveModal
+            onSave={(title: string): void => {
+              putMemo(title, text)
+              setShowModal(false)
+            }}
+            onCancel={() => setShowModal(false)}
+          />
+        )}
+    </>
+  )
+}
---
+++ src/pages/history.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react'
+import { Button } from '../components/button'
+import {
+  Link,
+  useHistory,
+} from 'react-router-dom'
+import styled from 'styled-components'
+import { Header } from '../components/header'
+import {
+  getMemos,
+  MemoRecord,
+} from '../indexeddb/memos'
+
+const { useState, useEffect } = React
+
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  left: 0;
+`
+
+const Wrapper = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 3rem;
+  padding: 0 1rem;
+`
+
+const Memo = styled.button`
+    display: block;
+    background-color: white;
+    border: 1px solid gray;
+    width: 100%;
+    padding: 1rem;
+    margin: 1rem 0;
+    text-align: left;
+  `
+  
+const MemoTitle = styled.div`
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`
+
+const MemoText = styled.div`
+  font-size: 0.85rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+interface Props {
+  setText: (text: string) => void
+}
+
+export const History: React.FC<Props> = (props) => {
+  const { setText } = props
+  const [memos, setMemos] = useState<MemoRecord[]>([])
+  const history = useHistory()
+  
+    useEffect(() => {
+      getMemos().then(setMemos)
+    }, [])
+  return (
+    <>
+      <HeaderArea>
+        <Header title="履歴">
+          <Link to="/editor">
+            エディタに戻る
+          </Link>
+        </Header>
+      </HeaderArea>
+      <Wrapper>
+        {memos.map(memo => (
+          <Memo
+            key={memo.datetime}
+            onClick={() => {
+              setText(memo.text)
+              history.push('/editor')
+            }}
+          >
+            <MemoTitle>{memo.title}</MemoTitle>
+            <MemoText>{memo.text}</MemoText>
+          </Memo>
+        ))}
+      </Wrapper>
+    </>
+  )
+}
